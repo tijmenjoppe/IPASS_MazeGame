038fc3fe38bf68927cb0bee28ecd6f2d886503ab
Sjoerd <sbeetsma99@gmail.com>
2021-06-04 17:29:45 +0200

    Change typo in init docstring

--- maze.py
+++ maze.py
@@ -2,10 +2,11 @@ import numpy as np
 import random
 import maze_generators
 
+
 class Maze:
     def __init__(self, x, y, gen_func=None):
         """
-        creates empty maze of size (y^2 + 1) * (x^2 +1)
+        creates empty maze of size (y*2 + 1) * (x*2 +1)
         y, x representing the number of nodes that will be placed on uneven rows/cols (0-visited 1-not-visited).
         Even rows/cols will represent edges/walls (1).
         The maze starts out as a grid filled with Ones and the passages will be carved out
@@ -22,26 +23,23 @@ class Maze:
         self.graph = np.ones((self.rows, self.cols)).astype(int)
         # start is at left up corner finish at right down
         self.start = (1, 1)
-        self.end = (self.rows-2, self.cols-2)
-        gen_funcs[gen_func](self)
+        self.end = (self.rows - 2, self.cols - 2)
+        if gen_func:
+            gen_funcs[gen_func](self)
 
     def neighbors(self, xpos, ypos, shuffle=True):
         # list comprehension to return neighbors. Only returns neighbors existent in the graph.
         # ex: cell 1, 1 only has 2 neighbors
-        n = [*[(xpos + i, ypos) for i in range(-2, 3, 4) if 0 <= xpos + i <= self.rows - 1],
-             *[(xpos, ypos + i) for i in range(-2, 3, 4) if 0 <= ypos + i <= self.cols - 1]]
+        n = [*[(xpos + i, ypos) for i in range(-2, 3, 4) if 0 < xpos + i < self.rows - 1],  # and 0 < ypos <= self.cols
+             *[(xpos, ypos + i) for i in range(-2, 3, 4) if 0 < ypos + i < self.cols - 1]]  # and 0 < xpos <= self.rows
         if shuffle:
             random.shuffle(n)
         return n
 
     def destroy_wall(self, current_cell, neighbor_cell):
-        """destroys a wallcell between 2 given cells"""
+        """destroys a wall cell between 2 given cells"""
         self.graph[(current_cell[0] + neighbor_cell[0]) // 2][(current_cell[1] + neighbor_cell[1]) // 2] = 0
 
 
-m = Maze(3, 3, 'DFS')
+m = Maze(3, 3, 'AB')
 print(m.graph)
-print(m.end)
-
-print(m.graph)
-
