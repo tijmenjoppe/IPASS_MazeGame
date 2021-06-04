4ee84c360cdd238d083b100b55fe6a79f2ce9613
Sjoerd <sbeetsma99@gmail.com>
2021-06-04 11:41:48 +0200

    return shuffled list of neighbors instead of ordered. Change empty graph to 'full' graph to carve out the maze.

--- Maze.py
+++ Maze.py
@@ -11,25 +11,24 @@ class Maze:
         """
         self.rows = 2 * x + 1
         self.cols = 2 * y + 1
-        self.graph = np.zeros((self.rows, self.cols))
-        #for i in range(0, self.rows, 2):
-        #    self.graph[i, :] = 1
-        #for i in range(0, self.rows, 2):
-        #    self.graph[:, i] = 1
+        self.graph = np.ones((self.rows, self.cols))
 
-    def neighbors(self, xpos, ypos):
+    def neighbors(self, xpos, ypos, shuffle=True):
         """"function to get list of neighbors relative to a given xpos, ypos
         we only look for neighbors from and on the uneven x / y axis this is because all the even rows/cols are walls/edges
 
         params:
             x: x position
             y: y position
+            shuffle: True or False to shuffle the list of neighbors before return
         returns:
-            list of neighbor (x, y) positions
+            list of neighbor relative to (x, y) position
             """
 
         # list comprehension to return neighbors. Only returns neighbors existent in the graph.
         # ex: cell 1, 1 only has 2 neighbors
-        return [(xpos + i, ypos) for i in range(-2, 3, 4) if 0 <= xpos + i <= self.rows - 1 and self.graph[xpos+i][ypos]] + \
+        neighbor_lst = [(xpos + i, ypos) for i in range(-2, 3, 4) if 0 <= xpos + i <= self.rows - 1 and self.graph[xpos+i][ypos]] + \
                [(xpos, ypos + i) for i in range(-2, 3, 4) if 0 <= ypos + i <= self.cols - 1 and self.graph[xpos][ypos+i]]
+        random.shuffle(neighbor_lst)
+        return neighbor_lst
 
