a2ba3a3fda2b2c63ef4c9bfbbabc6d19f336bb56
Sjoerd <sbeetsma99@gmail.com>
2021-06-13 11:16:58 +0200

    Add wilson's algorithm to generate maze. Wilson's algorithm is extended by allowing a random walker (aldolous broder) run along. To re-use code a random_walker function is implemented that Wilson can use and aldolous broder does use.

--- maze/maze_generators.py
+++ maze/maze_generators.py
@@ -1,26 +1,23 @@
 import random
 import pygame
-def animation_setup_grid(grid, tile_size=10):
-    pygame.init()
-    clock = pygame.time.Clock()
-    tile_size = tile_size
-    win = pygame.display.set_mode((len(grid)*tile_size, len(grid[0])*tile_size))
+from maze.animate_helpers import draw_grid, animation_setup_grid
 
-    return clock, tile_size, win
-def draw_grid(win,grid, tile_size):
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-    cols = len(grid[0])
-    rows = len(grid)
-    for Y in range(cols):
-        for X in range(rows):
-            x = X * tile_size
-            y = Y * tile_size
-            if grid[X][Y] == 1:
-                pygame.draw.rect(win, (0, 0, 0), (y, x, tile_size, tile_size))
-            elif grid[X][Y] == 0:
-                pygame.draw.rect(win, (255,255, 255), (y, x, tile_size, tile_size))
+def random_walker(maze, current_cell, path=[]):
+    """random walker used in AB and AB WILSON combination
+    randomly walks along the maze and destroys the wall between the next and current cell if the next cell is not visited yet
+    a path can be given to avoid (only for ab wilson)"""
+
+    next_cell = maze.neighbors(current_cell[0], current_cell[1], True)[0]
+    if next_cell in path:
+        return current_cell
+
+    if next_cell in maze.not_visited_cells.copy():
+        maze.destroy_wall(current_cell, next_cell)
+        maze.grid[next_cell] = 0
+        maze.not_visited_cells.remove(next_cell)
+
+    return next_cell
+
 
 def depth_first_search(maze, start=(1, 1), animate=False):
     """First implemented recursively changed to iterive implementation to avoid recursion depth errors
@@ -29,7 +26,7 @@ def depth_first_search(maze, start=(1, 1), animate=False):
     def draw():
         draw_grid(win, maze.grid, tile_size)
         for pos in stack:
-            pygame.draw.rect(win, (255, 0, 0), (pos[1] * tile_size, pos[0] * tile_size, tile_size, tile_size))
+            pygame.draw.rect(win, (255, 0, 0), (pos[0] * tile_size, pos[1] * tile_size, tile_size, tile_size))
         pygame.display.flip()
     if animate:
         clock, tile_size, win = animation_setup_grid(maze.grid)
@@ -39,8 +36,6 @@ def depth_first_search(maze, start=(1, 1), animate=False):
     # default starting cell is first (uneven!) cell in the graph
     maze.grid[start] = 0
     while stack:
-        if animate:
-            draw()
         # current cell
         current_cell = stack[-1]
         # find neighbors and filter out the already visited neighbors (value on graph is 0)
@@ -54,48 +49,43 @@ def depth_first_search(maze, start=(1, 1), animate=False):
             # destroy wall between neighbor and current cell
             maze.destroy_wall(current_cell, next_cell)
             stack.append(next_cell)
+        if animate:
+            draw()
+            clock.tick(animate)
 
 
 def aldous_broder(maze, start=(1, 1), animate=False):
     def draw():
         draw_grid(win, maze.grid, tile_size)
-        pygame.draw.rect(win, (0, 255, 0),
-                         (current_cell[1] * tile_size, current_cell[0] * tile_size, tile_size, tile_size))
+        pygame.draw.rect(win, (255, 0, 0),
+                         (current_cell[0] * tile_size, current_cell[1] * tile_size, tile_size, tile_size))
         pygame.display.flip()
     if animate:
         clock, tile_size, win = animation_setup_grid(maze.grid)
-
-    visited_cells = [start]
+    # ---------------------
     current_cell = start
     maze.grid[current_cell] = 0
-    while len(visited_cells) < (maze.x * maze.y):
+    maze.not_visited_cells.remove(current_cell)
+    while len(maze.not_visited_cells) > 0:
+        current_cell = random_walker(maze, current_cell)
+        # ---------------------
         if animate:
             draw()
-        neighbors = maze.neighbors(current_cell[0], current_cell[1], True)
-        next_cell = neighbors[0]
-        if maze.grid[next_cell]:
-            visited_cells.append(next_cell)
-            maze.destroy_wall(current_cell, next_cell)
-            maze.grid[next_cell] = 0
-        current_cell = next_cell
+            clock.tick(animate)
 
-def prim(maze, start=(1, 1), animate=False):
-    print("prim")
+def prim(maze, start=(1,1), animate=False):
     def draw():
         draw_grid(win, maze.grid, tile_size)
         for pos in frontier:
-            pygame.draw.rect(win, (255, 0, 0), (pos[1] * tile_size, pos[0] * tile_size, tile_size, tile_size))
-        pygame.display.flip()
-
+            pygame.draw.rect(win, (255, 0, 0), (pos[0] * tile_size, pos[1] * tile_size, tile_size, tile_size))
     if animate:
         clock, tile_size, win = animation_setup_grid(maze.grid)
+    # ---------------------
     frontier = set()
     maze.grid[start] = 0
     for cell in maze.neighbors(start[0], start[1]):
         frontier.add(cell)
     while frontier:
-        if animate:
-            draw()
         random_front = random.sample(frontier, 1)[0]
         connected_cell = [n for n in maze.neighbors(random_front[0], random_front[1]) if maze.grid[n] == 0]
         maze.destroy_wall(connected_cell[0], random_front)
@@ -104,5 +94,45 @@ def prim(maze, start=(1, 1), animate=False):
         for front in new_fronts:
             frontier.add(front)
         frontier.remove(random_front)
+        # ---------------------
+        if animate:
+            draw()
+            clock.tick(animate)
+            pygame.display.flip()
+def wilson(maze, start=(1, 1), animate=False, extra_walker = False):
+    def draw():
+        draw_grid(win, maze.grid, tile_size)
+        for pos in path:
+            pygame.draw.rect(win, (255, 0, 0), (pos[0] * tile_size, pos[1] * tile_size, tile_size, tile_size))
+        if extra_walker:
+            pygame.draw.rect(win, (255, 0, 0),
+                             (extra_walker_pos[0] * tile_size, extra_walker_pos[1] * tile_size, tile_size, tile_size))
+        pygame.display.flip()
 
+    if animate:
+        clock, tile_size, win = animation_setup_grid(maze.grid)
+    start = random.sample(maze.not_visited_cells, 1)[0]
+    extra_walker_pos = start
+    maze.not_visited_cells.remove(start)
+    maze.grid[start] = 0
 
+    while len(maze.not_visited_cells) > 0:
+        path = [random.sample(maze.not_visited_cells, 1)[0]]
+        next_cell = path[-1]
+
+        while next_cell in maze.not_visited_cells:
+            next_cell = maze.neighbors(next_cell[0], next_cell[1], True)[0]
+            if next_cell in path:
+                path = path[:path.index(next_cell)]
+            path.append(next_cell)
+            if extra_walker:
+                extra_walker_pos = random_walker(maze, extra_walker_pos, path)
+
+            if animate:
+                clock.tick(animate)
+                draw()
+
+        for i, pos in enumerate(path[:-1]):
+            maze.grid[pos] = 0
+            maze.destroy_wall(pos, path[i + 1])
+            maze.not_visited_cells.remove(pos)
