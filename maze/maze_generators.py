16939920b006d6e70606c5712e806030be0efc86
Sjoerd <sbeetsma99@gmail.com>
2021-06-14 15:37:02 +0200

    add in function documentation to generation in addition to existing docstrings

--- maze/maze_generators.py
+++ maze/maze_generators.py
@@ -3,6 +3,7 @@ import pygame
 from maze.animate_helpers import draw_grid, animation_setup_grid
 from rgb_colors import *
 
+
 def random_walker(maze, current_cell, path=[]):
     """random walker used in Aldolous Broder and Wilson's algorithm if chosen to use a extra walker.
     randomly walks (once per function calL) along any cells of the maze. If the cell hasn't been visited yet the passages gets carved out.
@@ -89,7 +90,7 @@ def aldous_broder(maze, start=(1, 1), animate=False):
 
     if animate:
         clock, tile_size, win = animation_setup_grid(maze.grid)
-
+    # start at the start position and mark that cell as part of the maze / visited (0)
     current_cell = start
     maze.grid[current_cell] = 0
     maze.not_visited_cells.remove(current_cell)
@@ -119,20 +120,27 @@ def prim(maze, start=(1, 1), animate=False):
 
     if animate:
         clock, tile_size, win = animation_setup_grid(maze.grid)
-
+    # set of all cells that are neighbors of cells that have been visited but not visited themself.
     frontier = set()
+    # start at the start position and mark that cell as part of the maze / visited (0)
     maze.grid[start] = 0
+    # add the neighbors of the starting cell to the frontier
     for cell in maze.neighbors(start[0], start[1]):
         frontier.add(cell)
+    # algorithm finishes when frontier is empty: all cells have been visited.
     while frontier:
-        random_front = random.sample(frontier, 1)[0]
-        connected_cell = [n for n in maze.neighbors(random_front[0], random_front[1]) if maze.grid[n] == 0]
-        maze.destroy_wall(connected_cell[0], random_front)
-        maze.grid[random_front] = 0
-        new_fronts = [n for n in maze.neighbors(random_front[0], random_front[1]) if maze.grid[n] == 1]
+        # choose a random next cell from the frontier
+        random_frontier_cell = random.sample(frontier, 1)[0]
+        # get a cell that is already in the maze and is also a neighbor of the frontier cell
+        connected_cell = [n for n in maze.neighbors(random_frontier_cell[0], random_frontier_cell[1]) if maze.grid[n] == 0]
+        # destroy wall between current cell and next cell and mark the random frontier cell as part of the maze (0)
+        maze.destroy_wall(connected_cell[0], random_frontier_cell)
+        maze.grid[random_frontier_cell] = 0
+        # get neighbor cells of the random frontier cell that aren't in the maze yet and add them to the frontier.
+        new_fronts = [n for n in maze.neighbors(random_frontier_cell[0], random_frontier_cell[1]) if maze.grid[n] == 1]
         for front in new_fronts:
             frontier.add(front)
-        frontier.remove(random_front)
+        frontier.remove(random_frontier_cell)
 
         if animate:
             draw(), clock.tick(animate)
@@ -154,30 +162,40 @@ def wilson(maze, extra_walker=False, animate=False):
         for pos in path:
             pygame.draw.rect(win, RED, (pos[0] * tile_size, pos[1] * tile_size, tile_size, tile_size))
         if extra_walker:
-            pygame.draw.rect(win, RED, (extra_walker_pos[0] * tile_size, extra_walker_pos[1] * tile_size, tile_size, tile_size))
+            pygame.draw.rect(win, RED,
+                             (extra_walker_pos[0] * tile_size, extra_walker_pos[1] * tile_size, tile_size, tile_size))
         pygame.display.flip()
 
-    if animate: clock, tile_size, win = animation_setup_grid(maze.grid)  # only when animated
-
+    if animate: clock, tile_size, win = animation_setup_grid(maze.grid)
+    # pick a random start cell
     start = random.sample(maze.not_visited_cells, 1)[0]
-    extra_walker_pos = start
+    # if extra_walker is activated the walk will start from this start position
+    if extra_walker:
+        extra_walker_pos = start
+    # remove the start position from not_visited_cells and mark it as part of the maze (0)
     maze.not_visited_cells.remove(start)
     maze.grid[start] = 0
 
+    # while there are unvisited cells left
     while len(maze.not_visited_cells) > 0:
+        # start the path at a random location
         path = [random.sample(maze.not_visited_cells, 1)[0]]
-        next_cell = path[-1]
-
+        # current cell is the first element in the path
+        next_cell = path[0]
+        # while next_cell is not part of the maze keep randomly walking (without changing the maze)
         while next_cell in maze.not_visited_cells:
             next_cell = maze.neighbors(next_cell[0], next_cell[1], True)[0]
+            # if the next_cell is in the path a loop is created cut off the path at the point of the loop and continue from there
             if next_cell in path:
                 path = path[:path.index(next_cell)]
             path.append(next_cell)
+            # if extra walker is active make it go a next step
             if extra_walker:
                 extra_walker_pos = random_walker(maze, extra_walker_pos, path)
             if animate:
                 clock.tick(animate), draw()
-
+        # for loop is reached when the path reaches a cell that is part of the maze(0)
+        # the positions in the path and walls between them get destroyed.
         for i, pos in enumerate(path[:-1]):
             maze.grid[pos] = 0
             maze.destroy_wall(pos, path[i + 1])
